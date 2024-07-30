706a82ab161844b0212c56ffd308ce4704029774
KutayKoray <kutaykoray58@gmail.com>
2024-07-30 15:07:45 +0300

    chore: Add database configuration and models

---
+++ Models/Listing.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sahibinden_project;
+
+public class Listing
+{
+    [Key]
+    public int Id { get; set; }
+    public string? Category { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? Date { get; set; }
+    public string? ImageFileName { get; set; }
+
+}
